2d9d399acae570ad31c401d52ebbe30d0f4c5437
Asmozan <asmozan@mail.ru>
2017-02-25 21:40:46 +0100

    ADD: error log file, error output

--- compile.sh
+++ compile.sh
@@ -7,10 +7,16 @@ echo "add_executable(main main.cpp)" >> CMakeLists.txt
 echo "set(CMAKE CXX_FLAGS "-Wall -Werror -std=c+11")" >> CMakeLists.txt
 
 #compile files
+rm -r out
 mkdir out
 cd out
 cmake ..
 
 #make compile great again
 make help
-make
+make 2> error.log
+cat error.log | grep "error"
+
+#result show
+cd ..
+bash resultlog.sh
