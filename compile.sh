b7713a921a491ae9b51d3b2a56099f42fcc41358
Asmozan <asmozan@mail.ru>
2017-02-25 18:14:50 +0100

    ADD: out dir creator , compile from CML

--- compile.sh
+++ compile.sh
@@ -1,7 +1,13 @@
+#CMakeLists init & config
 touch CMakeLists.txt
 echo "cmake_minimum_required(VERSION 2.8)" > CMakeLists.txt
 echo "project(main C CXX)" >> CMakeLists.txt
 
 echo "add_executable(main main.cpp)
 echo "set(CMAKE CXX_FLAGS "-Wall -Werror -std=c+11")"
+
+#compile files
+mkdir out
+cd out
+cmake ..
 
