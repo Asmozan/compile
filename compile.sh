50cf5adf6c1a238343951726fbaa33b45bb52258
Asmozan <asmozan@mail.ru>
2017-02-25 18:21:55 +0100

    MOD: Missing " fixed

--- compile.sh
+++ compile.sh
@@ -3,8 +3,8 @@ touch CMakeLists.txt
 echo "cmake_minimum_required(VERSION 2.8)" > CMakeLists.txt
 echo "project(main C CXX)" >> CMakeLists.txt
 
-echo "add_executable(main main.cpp)
-echo "set(CMAKE CXX_FLAGS "-Wall -Werror -std=c+11")"
+echo "add_executable(main main.cpp)"
+echo "set$(CMAKE CXX_FLAGS "-Wall -Werror -std=c+11")"
 
 #compile files
 mkdir out
