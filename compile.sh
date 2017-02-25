41d0baee3bd86f54c654ce5fbb2b98ec486e8c4e
Asmozan <asmozan@mail.ru>
2017-02-25 22:02:28 +0100

    MOD: disc update, excluded C from project type

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
+## Compile version for c++
 #CMakeLists init & config
 touch CMakeLists.txt
 echo "cmake_minimum_required(VERSION 2.8)" > CMakeLists.txt
-echo "project(main C CXX)" >> CMakeLists.txt
+echo "project(main CXX)" >> CMakeLists.txt
 
 echo "add_executable(main main.cpp)" >> CMakeLists.txt
 echo "set(CMAKE CXX_FLAGS "-Wall -Werror -std=c+11")" >> CMakeLists.txt
