3df315b0fc4fcc6836ac8a48b7cf96fa9a36dd97
Asmozan <asmozan@mail.ru>
2017-02-25 18:32:27 +0100

    MOD: echo corretly prints to file , removed $

--- compile.sh
+++ compile.sh
@@ -3,11 +3,14 @@ touch CMakeLists.txt
 echo "cmake_minimum_required(VERSION 2.8)" > CMakeLists.txt
 echo "project(main C CXX)" >> CMakeLists.txt
 
-echo "add_executable(main main.cpp)"
-echo "set$(CMAKE CXX_FLAGS "-Wall -Werror -std=c+11")"
+echo "add_executable(main main.cpp)" >> CMakeLists.txt
+echo "set(CMAKE CXX_FLAGS "-Wall -Werror -std=c+11")" >> CMakeLists.txt
 
 #compile files
 mkdir out
 cd out
 cmake ..
 
+#make compile great again
+make help
+make
